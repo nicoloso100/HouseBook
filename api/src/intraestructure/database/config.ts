069b47932c17c730117401bd4e8da4f3b127fe1e
teamzz111 <felipeflogxd@gmail.com>
2020-08-30 18:19:42 -0500

    database error log

--- api/src/intraestructure/database/config.ts
+++ api/src/intraestructure/database/config.ts
@@ -23,5 +23,7 @@ export const StartDatabase = () => {
         });
       console.log("Se ha conectado satisfactoriamente con la base de datos.");
     })
-    .catch(() => console.log("No se ha podido iniciar la base de datos."));
+    .catch((e) =>
+      console.log(`No se ha podido iniciar la base de datos.\n\n ${e} \n\n`)
+    );
 };
