df52a089d1af6dc665047e04efa49997e665822a
nicoloso100 <nico.las0315@hotmail.com>
2020-09-05 00:20:15 -0500

    docker local mongo connection string

--- api/src/intraestructure/database/contants.ts
+++ api/src/intraestructure/database/contants.ts
@@ -1,5 +1,6 @@
-export const MONGODB_CONNECTION_STRING =
-  "mongodb://root:HouseBook123@ec2-54-197-125-22.compute-1.amazonaws.com:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+export const MONGODB_CONNECTION_STRING = process.env.MONGODB
+  ? process.env.MONGODB
+  : "";
 
 export const mongoConfig = {
   database: MONGODB_CONNECTION_STRING,
