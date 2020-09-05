fa1709ae9aede31e45f483dc38dcab757cd0ef18
nicoloso100 <nico.las0315@hotmail.com>
2020-09-04 21:53:01 -0500

    changed mongo connection string

--- api/src/intraestructure/database/contants.ts
+++ api/src/intraestructure/database/contants.ts
@@ -1,5 +1,5 @@
 export const MONGODB_CONNECTION_STRING =
-  "mongodb://root:HouseBook123@localhost/HouseBook?authSource=admin";
+  "mongodb://root:HouseBook123@ec2-54-197-125-22.compute-1.amazonaws.com:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 export const mongoConfig = {
   database: MONGODB_CONNECTION_STRING,
