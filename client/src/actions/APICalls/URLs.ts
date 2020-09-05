766eb7a2f87524943bcf0ddff5a205bb43772c87
nicoloso100 <nico.las0315@hotmail.com>
2020-09-05 01:37:46 -0500

    base url client fix

--- client/src/actions/APICalls/URLs.ts
+++ client/src/actions/APICalls/URLs.ts
@@ -1,6 +1,6 @@
 import { IsDevelopment } from "utils/environment";
 
-export const baseURL = IsDevelopment ? "http://localhost:4000/api/" : "/api";
+export const baseURL = IsDevelopment ? "http://localhost:4000/api/" : "/api/";
 
 export const authURLs = {
   singIn: `${baseURL}auth/singin`,
